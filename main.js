51cefbf7c3d0f17b4b5dfbd0012cd21fd2806c4d
adeline ad <adeline.aiguier@gmail.comine@hotmail.com>
2024-03-19 09:08:31 +0100

    skills

--- main.js
+++ main.js
@@ -64,10 +64,10 @@ buttonsWrapper.addEventListener("click", e => {
       slides.style.transform = "translateX(-0%)";
       e.target.classList.add("active");
     } else if (e.target.classList.contains("second")) {
-      slides.style.transform = "translateX(-33.33333333333333%)";
+      slides.style.transform = "translateX(-40%)";
       e.target.classList.add("active");
     } else if (e.target.classList.contains('third')){
-      slides.style.transform = 'translatex(-66.6666666667%)';
+      slides.style.transform = 'translatex(-80%)';
       e.target.classList.add('active');
     }
   }
